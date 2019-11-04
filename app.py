6538337dc12ca00ff00d67f42ece16cf31401d0c
Juhana <juhan.niskanen@gmail.com>
2019-11-04 22:24:14 +0200

    modified app file

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ headers = {
 response = requests.get(
     'https://koodihaaste-api.solidabis.com/bullshit', headers=headers)
 json_data = json.loads(response.text)
-json_data['bullshits']
+
 
 
 words = [line.rstrip('\n').upper()
@@ -28,7 +28,6 @@ word_list = []
 m = 0
 
 for messages in json_data['bullshits']:
-    m += 1
     message = messages['message'].upper().replace('.', '')
     for key in range(len(LETTERS)):
         translated = ''
@@ -44,40 +43,40 @@ for messages in json_data['bullshits']:
                 translated = translated + symbol
                 translated_word_list = list(translated.split(' '))
         results.append([m, key, message, translated, *translated_word_list])
-        word_list.append([m, key, *translated_word_list])
-
+        word_list.append([m, key, message, translated, *translated_word_list])
+    m += 1
 maxxx = max(len(l) for l in word_list)
 
 columns = ['w' + str(i) for i in range(maxxx)]
 df_words = pd.DataFrame(word_list, columns=columns)
 df_words.fillna(value=0, inplace=True)
-df_words['summa'] = df_words.iloc[:, 2:maxxx].isin(
+df_words['summa'] = df_words.iloc[:, 4:maxxx].isin(
     words).sum(axis=1, skipna=True)
-# df_translated= df[df['summa']>=2].iloc[:,3].drop_duplicates()
-# df_jibberish = df[df['summa']<2].iloc[:,2].drop_duplicates()
-df_words.set_index(['w0', 'w1'], inplace=True)
+
+df_words_translated = df_words[df_words['summa'] > 3].loc[:,['w0','w3']]
+df_words_translated.columns = ['#','Translated']
+df_words_jibberish = df_words[~df_words['w0'].isin(df_words_translated['#'])].loc[:,['w0','w2']].drop_duplicates()
+df_words_jibberish.columns = ['#','Jibberish']
 
 
-print(df_words[df_words['summa'] > 2])
-print(df_words)
-print(max(len(l) for l in word_list))
-
-df_words[df_words['summa'] > 2].to_csv("output.csv")
+# df_words_translated.to_csv("translated.csv")
+# df_words_jibberish.to_csv("jibberish.csv")
 
 
-# with pd.option_context('display.max_rows', -1, 'display.max_columns', 5):
+@app.route('/')
+def index():
+    response = requests.get('https://koodihaaste-api.solidabis.com/bullshit', headers=headers)
+    data = json.loads(response.content)
+    return render_template('content.html', data = data)
+
+@app.route('/SomeFunction')
+def SomeFunction():
+    #return render_template('index.html', data_t = df_words_translated, data_j = df_words_jibberish)
+    return render_template('testi.html',tables=[df_words_translated.to_html(classes=
+    ["table-info","th text-center","thead thead-dark"],index=False),
+    df_words_jibberish.to_html(classes=["table-danger","th text-center","thead thead-dark"],index=False)],
+    titles = ['na','Translated','Jibberish'])
 
 
-# @app.route('/')
-# def index():
-#     response = requests.get('https://koodihaaste-api.solidabis.com/bullshit', headers=headers)
-#     data = json.loads(response.content)
-#     return render_template('content.html', data = data)
-
-# @app.route('/SomeFunction')
-# def SomeFunction():
-#     return render_template('index.html', data_t = df_translated, data_j = df_jibberish)
-
-
-# if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+   app.run()
